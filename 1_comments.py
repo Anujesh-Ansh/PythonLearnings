a6d650ddd1b03011f4d8d14844c921595bc0885f
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-25 19:24:40 +0530

    2nd commit

---
+++ 1_comments.py
@@ -0,0 +1,12 @@
+# COMMENTS
+
+# Comments are used in order to keep a code, which might be used in the future, but not of any use right now,
+# and also to explain the code
+
+# Comments in Python are written using '#'
+
+# for eg.
+
+print('Comment Below:- ')
+# print('Hello...')
+print('Comment Above:- ')
