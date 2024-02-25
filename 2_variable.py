09a229780b305cd689b7fd73580902323ab2141d
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-25 19:33:19 +0530

    3rd commit

--- 2_variable.py
+++ 2_variable.py
@@ -1,12 +1,40 @@
 # Variables
+# are used to store data, and are used to store data in a memory location, which can be used later in the code.
 
+# Variables in Python are declared using the following syntax:
+variable_name = 'value'
+
+# Different types of Datatypes are:
+
+# Integers
 a = 3
 print(a)
 
-b = 55
+# Float
+b = 6.2
 print(b)
 
-a = 6.2
-print(a)
+# Boolean
+c = True
+print(c)
 
-c = True  # or False ->
+# String
+d = 'Hello World'
+print(d)
+
+# Complex
+e = 3 + 5j
+print(e)
+
+# None
+f = None
+print(f)
+
+# List - ordered and changeable collection of items
+g = ['apple', 'banana', 'cherry']
+h = [('apple', 5), ('banana', 6), ('cherry', 7)]
+i = [1, 2, 3, 4, 5]
+
+print(g)
+print(h)
+print(i)
