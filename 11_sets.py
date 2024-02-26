e7484ee3a98c9af331684f71b27101e33fd00fdc
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-26 09:23:09 +0530

    12th commit

---
+++ 11_sets.py
@@ -0,0 +1,14 @@
+# Sets
+
+# A set is a collection which is unordered and unindexed. In Python sets are written with curly brackets. 
+# They can hold any object, but the objects must be immutable, i.e., they cannot be changed. 
+# Sets are used to perform mathematical set operations like union, intersection, symmetric difference, etc.
+
+# Example:
+# Creating a set -> { } or set()
+
+a1 = {1,2,3,3,4}
+print(a1)  # Output: {1, 2, 3, 4}
+
+a2 = {3,2,4}
+print(a2)  # Output: {2, 3, 4}
