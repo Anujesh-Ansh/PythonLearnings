b2375204fc03308dff2190d0f38ae6ab4f2bce09
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-27 00:53:06 +0530

    16th Commit

---
+++ 15_for-loop.py
@@ -0,0 +1,17 @@
+# For Loop
+# This is a simple for loop that iterates over a list of numbers and prints each number.
+
+# Syntax:
+# for variable in sequence:
+    # code to execute (usually an indented block)
+
+for i in range(5):
+    print(i) # Output: 0 1 2 3 4
+
+
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    print(num) # Output: 1 2 3 4 5
+
+
+
