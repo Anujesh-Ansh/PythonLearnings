8bbca936c43d070be999ed431210da7c0fe68ad2
Anujesh Ansh <anshanujesh@gmail.com>
2024-02-26 09:19:11 +0530

    2nd Commit

--- 2_variable.py
+++ 2_variable.py
@@ -4,6 +4,10 @@
 # Variables in Python are declared using the following syntax:
 variable_name = 'value'
 
+# Note:
+# Variable names can contain letters, numbers, and underscores. They cannot start with a number or special character.
+# Variable names are case-sensitive. This means that "Name" and "name" are two different variables.
+
 # Different types of Datatypes are:
 
 # Integers
@@ -22,6 +26,15 @@ print(c)
 d = 'Hello World'
 print(d)
 
+a1 = 'harry' # Single quotes can be used to write strings in Python
+a2 = "harry" # Both single and double quotes can be used to write strings in Python
+a3 = '''harry''' # Triple quotes can be used to write multi-line strings, like:
+
+a4 = '''harry
+is a good boy'''
+
+print(a4)
+
 # Complex
 e = 3 + 5j
 print(e)
@@ -38,3 +51,4 @@ i = [1, 2, 3, 4, 5]
 print(g)
 print(h)
 print(i)
+
